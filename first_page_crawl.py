019a2358196bf13b8b2ab137f095dc02b86de208
Karl Jonsson <karl.karbin@gmail.com>
2020-11-19 16:28:42 +0100

    refactoring, connected GUI

--- first_page_crawl.py
+++ first_page_crawl.py
@@ -6,68 +6,65 @@ import requests
 import os
 import time
 
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-MY_FILE = os.path.join(THIS_FOLDER, 'links.txt')
-info = Informer
+class FirstPageCrawl:
+    def __init__(self, kommun, omrade):
+        self.__kommun = kommun
+        self.__omrade = omrade
+        self.__THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+        self.__MY_FILE = os.path.join(self.__THIS_FOLDER, 'links.txt')
+        self.__info = Informer
 
-iterationCounter = 0
+    def __getAllLinks(self, URL: str):
+        links = []
+        front_page = requests.get(URL, timeout = 5)
+        soup = BeautifulSoup(front_page.text, "html.parser")
+        containers = soup.findAll("a", class_="js-listing-card-link listing-card")
+        for container in containers[1:]:
+            links.append(container.get("href"))
+        return links
 
-def getAllLinks(URL: str):
-    links = []
-    front_page = requests.get(URL, timeout = 5)
-    soup = BeautifulSoup(front_page.text, "html.parser")
-    containers = soup.findAll("a", class_="js-listing-card-link listing-card")
-    for container in containers[1:]:
-        links.append(container.get("href"))
-    return links
+    def __createURL(self, kommun: str, omrade: str):
+        base_URL = "https://www.hemnet.se/till-salu/lagenhet/"
+        if not kommun: #change from mock to real value
+            omrade = "" 
+        URL_trail = "?by=creation&housing_form_groups[]=apartments&order=desc&preferred_sorting=true" #queries for "nyast först"
+        query_URL = base_URL+kommun+omrade+URL_trail
+        return query_URL
 
-def createURL(kommun: str, omrade: str):
-    base_URL = "https://www.hemnet.se/till-salu/lagenhet"
-    if not kommun: #change from mock to real value
-        omrade = "" 
-    URL_trail = "?by=creation&housing_form_groups[]=apartments&order=desc&preferred_sorting=true" #queries for "nyast först"
-    query_URL = base_URL+kommun+omrade+URL_trail
-    return query_URL
+    def initializeCrawl(self):
+        self.__URL = self.__createURL(self.__kommun, self.__omrade)
+        print("Watching for new objects under " + self.__URL)
+        self.__iteration_counter = 0
 
-mock_kommun = "/taby-kommun"
-mock_omrade = ""
-URL = createURL(mock_kommun, mock_omrade)
-print("Watching for new objects under " + URL)
+    def runCrawl(self):
+        __checkedLinks = []
 
-while True:
+        with open(self.__MY_FILE, "r") as file:
+            contents = file.readlines()
+            for line in contents:
+                __checkedLinks.append(line[:-1])
+        file.close
 
-    checkedLinks = []
+        links = self.__getAllLinks(self.__URL)
 
-    with open(MY_FILE, "r") as file:
-        contents = file.readlines()
-        for line in contents:
-            checkedLinks.append(line[:-1])
-    file.close
+        __results = []
 
-    links = getAllLinks(URL)
+        for link in links:
+            if link not in __checkedLinks:
+                __results.append(link)
 
-    results = []
+        if len(links) > 0:
+            with open(self.__MY_FILE, "w") as file:
+                file.writelines("%s\n" % link for link in links)
 
-    for link in links:
-        if link not in checkedLinks:
-            results.append(link)
+        count = 0
+        for result in __results:
+            count += 1
+            print(result)
+        
+        print(str(count) + " new objects found.")
+        self.__iteration_counter += 1
+        print("HemnetWatcher has ran " + str(self.__iteration_counter) + " times since last restart.")
 
-    if len(links) > 0:
-        with open(MY_FILE, "w") as file:
-            file.writelines("%s\n" % link for link in links)
-
-    count = 0
-    for result in results:
-        count += 1
-        print(result)
-    
-    print(str(count) + " new objects found.")
-    iterationCounter += 1
-    print("HemnetWatcher has ran " + str(iterationCounter) + " times since last restart.")
-
-    if results:
-        if mock_omrade == "":
-            mock_omrade = " "
-        info.mail(info, results, mock_kommun, mock_omrade)
-
-    time.sleep(10)
+        if __results:
+            self.__info.mail(self.__info, __results, self.__kommun, self.__omrade)
---
+++ tester.py
@@ -0,0 +1,5 @@
+from first_page_crawl import FirstPageCrawl
+
+fpc = FirstPageCrawl("taby-kommun", "")
+fpc.initializeCrawl()
+fpc.runCrawl()
